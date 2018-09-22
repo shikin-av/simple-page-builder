0330b8718d41928be9303a4e9fdffb1d8a6aac7d
shikin-av <shikin.a.v@mail.ru>
2018-09-22 15:24:22 +0300

    themization

--- src/index.js
+++ src/index.js
@@ -2,8 +2,46 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from 'components/App'
 
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import grey from '@material-ui/core/colors/grey'
+import orange from '@material-ui/core/colors/orange'
+import lime from '@material-ui/core/colors/lime'
+
+import fake from 'components/fake'
+
+const getPageRows = () => {
+    return fake
+}
+
 const save = rows => {
     console.log('SAVE', rows)
 }
 
-ReactDOM.render(<App saveHandler={save}/>, document.getElementById('root'))
+const orangeNightTheme =  createMuiTheme({
+    palette: {
+        primary:   {main: orange[500]},
+        secondary: {main: orange[300]},
+        background:       grey[700],
+        contrast:         grey[100],
+    },
+})
+
+const whiteGreenTheme = createMuiTheme({
+    palette: {
+        primary:   {main: lime[500]},
+        secondary: {main: lime[300]},
+        background:       grey[50],
+        contrast:         grey[700],
+    },
+})
+
+window.theme = whiteGreenTheme
+
+ReactDOM.render(
+    <App 
+        saveHandler={save}
+        menu={true}
+        getPageRows={getPageRows}
+    />, 
+    document.getElementById('root')
+)
