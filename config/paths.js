376e74b0651640bf1fa4598aca1b1ecfe13f41b9
shikin-av <shikin.a.v@mail.ru>
2018-09-27 20:24:39 +0300

    'index'(App component) and 'demo'(page)

--- config/paths.js
+++ config/paths.js
@@ -45,6 +45,7 @@ module.exports = {
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveApp('src/index.js'),
+  demoIndexJs: resolveApp('src/demo.js'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
   yarnLockFile: resolveApp('yarn.lock'),
